31f08fe1dda335ab43d09f09e080becd951d6818
ViktorVojtek <vvojtek@enli.sk>
2020-04-06 23:02:37 +0200

    Work on product create in progress

---
+++ client/components/ProductCreateForm/components/DataSelect/index.js
@@ -0,0 +1,76 @@
+/* eslint-disable react/forbid-prop-types */
+import React from 'react';
+import { useQuery } from '@apollo/react-hooks';
+
+import { Input, FormGroup } from 'reactstrap';
+import PropTypes from 'prop-types';
+
+const DynamicSelect = ({
+  query, category, onSelect, productData,
+}) => {
+  const { loading, error, data } = useQuery(query);
+
+  if (loading) {
+    return <>loading</>;
+  }
+
+  if (error) {
+    return <>{error.message}</>;
+  }
+
+  const handleOnChange = (event) => {
+    const { id } = event.currentTarget.options[event.currentTarget.selectedIndex];
+    let selectorData;
+
+    if (category) {
+      selectorData = { ...productData, category: id };
+    } else {
+      selectorData = { ...productData, subCategory: id };
+    }
+
+    onSelect(selectorData);
+  };
+
+  const dataArr = category ? data.categories : data.subCategories;
+
+  return (
+    <FormGroup>
+      <Input
+        type="select"
+        onChange={handleOnChange}
+      >
+        <option>Select</option>
+        {
+          dataArr && dataArr.length > 0
+            ? (
+              dataArr.map(({ _id, title }) => (
+                <option key={_id} id={_id}>{title}</option>
+              ))
+            ) : null
+        }
+      </Input>
+    </FormGroup>
+  );
+};
+
+DynamicSelect.defaultProps = {
+  category: false,
+};
+DynamicSelect.propTypes = {
+  query: PropTypes.object.isRequired,
+  category: PropTypes.bool,
+  onSelect: PropTypes.func.isRequired,
+  productData: PropTypes.shape({
+    category: PropTypes.string,
+    description: PropTypes.string,
+    discount: PropTypes.number,
+    modifiedByUserId: PropTypes.string,
+    shortDescription: PropTypes.string,
+    subCategory: PropTypes.string,
+    images: PropTypes.arrayOf(PropTypes.object),
+    note: PropTypes.string,
+    title: PropTypes.string,
+  }).isRequired,
+};
+
+export default DynamicSelect;
--- client/components/ProductCreateForm/index.js
+++ client/components/ProductCreateForm/index.js
@@ -1,19 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useMutation } from '@apollo/react-hooks';
 import {
   Button, Form, FormGroup, Label, Input,
 } from 'reactstrap';
 
 import { CREATE_PRODUCT_MUTATION } from '../../app-data/graphql/mutation';
-import { PRODUCT_QUERY } from '../../app-data/graphql/query';
+import {
+  PRODUCT_QUERY, CATEGORIES_QUERY, SUBCATEGORIES_QUERY,
+} from '../../app-data/graphql/query';
+
+import DynamicSelect from './components/DataSelect';
 
 const ProductCreateForm = () => {
+  const [productData, setProductData] = useState({});
   const [createProduct] = useMutation(
     CREATE_PRODUCT_MUTATION,
     {
       refetchQueries: [{ query: PRODUCT_QUERY }],
     },
   );
+
+  const handleSetProductData = (data) => {
+    setProductData(data);
+  };
 
   const handleSubmitProductData = async (event) => {
     event.preventDefault();
@@ -28,6 +37,8 @@ const ProductCreateForm = () => {
     }
   };
 
+  // console.log(productData);
+
   return (
     <Form onSubmit={handleSubmitProductData}>
       <h5>Products</h5>
@@ -38,6 +49,26 @@ const ProductCreateForm = () => {
           type="text"
           placeholder="Insert product title"
         />
+      </FormGroup>
+      <DynamicSelect
+        category
+        query={CATEGORIES_QUERY}
+        onSelect={handleSetProductData}
+        productData={productData}
+      />
+      <DynamicSelect
+        query={SUBCATEGORIES_QUERY}
+        onSelect={handleSetProductData}
+        productData={productData}
+      />
+      <FormGroup>
+        <Input type="textarea" id="shortDescription" name="shortDescription" placeholder="Short description" />
+      </FormGroup>
+      <FormGroup>
+        <Input type="textarea" id="description" name="description" placeholder="Description" />
+      </FormGroup>
+      <FormGroup>
+        <Input type="textarea" id="note" name="note" placeholder="Note" />
       </FormGroup>
       <FormGroup>
         <Button>Submit</Button>
