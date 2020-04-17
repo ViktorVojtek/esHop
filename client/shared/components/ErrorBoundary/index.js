1579f31521617ea3caa2cfa09937164cacb6f1e4
ViktorVojtek <vvojtek@enli.sk>
2020-04-17 13:42:03 +0200

    Modified .gitignore file to prevent pushing product images to the git

--- client/shared/components/ErrorBoundary/index.js
+++ client/shared/components/ErrorBoundary/index.js
@@ -4,7 +4,12 @@ import PropTypes from 'prop-types';
 class ErrorBoundary extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { hasError: false };
+
+    this.state = { 
+      hasError: false,
+      error: { message: '', stack: '' },
+      info: { componentStack: '' }
+     };
   }
 
   static propTypes = {
@@ -15,23 +20,28 @@ class ErrorBoundary extends React.Component {
   };
 
   static getDerivedStateFromError(error) {
-    console.log(error);
     // Update state so the next render will show the fallback UI.
     return { hasError: true };
   }
 
-  componentDidCatch(error, errorInfo) {
+  componentDidCatch(error, info) {
     // You can also log the error to an error reporting service
-    logErrorToMyService(error, errorInfo);
+    this.setState({ error, errorInfo });
   }
 
   render() {
-    const { hasError } = this.state;
+    const { hasError, error, info } = this.state;
     const { children } = this.props;
   
     if (hasError) {
       // You can render any custom fallback UI
-      return <h1>Something went wrong.</h1>;
+      return (
+        <h1>
+          Something went wrong.
+          {' '}
+          {error}
+        </h1>
+      );
     }
 
     return children; 
