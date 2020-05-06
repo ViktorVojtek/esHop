3efa2767165f180dced394b2821e3b85f37e3792
ViktorVojtek <viktor1vojtek@gmail.com>
2020-05-06 11:23:19 +0200

    Crrate cart page component

---
+++ pages/eshop/cart/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import Layout from '../../../app-data/shared/components/Layout/Site.layout';
+
+const CartPage: () => JSX.Element = () => (
+  <Layout>
+    <p>This is cart page</p>
+  </Layout>
+);
+
+export default CartPage;
