af2cd42a236853d0b94f88e1f83533f16518665b
ViktorVojtek <viktor1vojtek@gmail.com>
2020-06-02 11:04:14 +0200

    Fixed products query issue

--- server/app-data/graphql/resolvers/query/Products/index.ts
+++ server/app-data/graphql/resolvers/query/Products/index.ts
@@ -8,9 +8,9 @@ const Products: (
 ) => Promise<IProduct[]> = async (root, { categoryId, subCategoryId }, ctx) => {
   try {
     const result = subCategoryId
-      ? (await Product.find({ subCategory: subCategoryId })) || []
+      ? (await Product.find({ 'subCategory.id': subCategoryId })) || []
       : categoryId
-      ? (await Product.find({ category: categoryId })) || []
+      ? (await Product.find({ 'category.id': categoryId })) || []
       : (await Product.find()) || [];
 
     return result;
