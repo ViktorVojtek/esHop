fab3db72be83ff6d0582b3f961dc48ebbb797101
ViktorVojtek <viktor1vojtek@gmail.com>
2020-07-17 13:26:24 +0200

    Product update issue fix in progress

--- server/app-data/graphql/resolvers/mutation/UpdateProduct/index.ts
+++ server/app-data/graphql/resolvers/mutation/UpdateProduct/index.ts
@@ -23,61 +23,42 @@ const updateProduct: (
       throw new ModError(404, 'Product not exist.');
     }
 
-    console.log('Product exist.');
-    console.log('Input data are:');
-    console.log(productInput);
-    console.log('\n');
-    console.log('Finded product is:');
-    console.log(productExist);
-    console.log('\n');
-
     const { variants, ...restProductData } = productInput;
     const productData = { ...restProductData };
-
-    console.log('Cleaned data are: ');
-    console.log(productData);
-    console.log('\n');
 
     let i = 0;
     let variantsData = [];
 
     while (i < variants.length) {
-      const { images } = variants[i];
       let imagesData: any[] = [];
+      const resultVariant = { ...variants[i] };
 
-      if (images && images.length > 0 && images[0].base64) {
-        const vId = `${productData._id}-${variants[i].title.toUpperCase()}`;
+      if (variants[i].images && variants[i].images.length > 0) {
+        const { images } = variants[i];
 
-        imagesData = await getVariantImagesPaths(images, vId);
-      } else {
-        imagesData = productExist.variants[i].images;
+        if (images && images.length > 0 && images[0].base64) {
+          const vId = `${productData._id}-${variants[i].title.toUpperCase()}`;
+
+          imagesData = await getVariantImagesPaths(images, vId);
+        } else {
+          imagesData = productExist.variants[i].images;
+        }
+
+        resultVariant.images = imagesData;
       }
-
-      const resultVariant = {
-        ...variants[i],
-        images: imagesData,
-      };
 
       variantsData.push(resultVariant);
 
       i += 1;
     }
 
-    console.log('Variants data are:');
-    console.log(variantsData);
-    console.log('\n');
-
     productData.variants = variantsData;
-
-    console.log('Final data to update: ');
-    console.log(productData);
 
     const updatedProduct: IProduct = await Product.findOneAndUpdate(
       { _id: mongoose.Types.ObjectId(_id) },
       {
         $set: {
           category: productData.category,
-          // dateModified: (Date.now as unknown) as Date,
           subCategory: productData.subCategory,
           title: productData.title,
           variants: productData.variants,
@@ -87,9 +68,6 @@ const updateProduct: (
     );
 
     const { __v, ...result } = updatedProduct.toObject();
-
-    console.log('Updated result: ');
-    console.log(result);
 
     return result as IProduct;
   } catch (err) {
