794144f6baaf102e937ae7cb08c6fbeb9d1e580a
ViktorVojtek <viktor1vojtek@gmail.com>
2020-08-15 14:04:56 +0200

    Mail notification for successfuly placed order prepared.

--- server/app-data/graphql/resolvers/mutation/CreateOrder/index.ts
+++ server/app-data/graphql/resolvers/mutation/CreateOrder/index.ts
@@ -1,7 +1,46 @@
 import mongoose from 'mongoose';
+import nodemailer from 'nodemailer';
 import Customer, { ICustomer } from '../../../../db/models/Customer';
 import Order, { IOrder } from '../../../../db/models/Order';
 import ModError from '../../utils/error';
+
+// from: '"Fred Foo 👻" <foo@example.com>'
+// to: "bar@example.com, baz@example.com",
+
+function sendMailNotification(from: string, to: string) {
+  return new Promise(async (resolve, reject) => {
+    try {
+      // Generate test SMTP service account from ethereal.email
+      // Only needed if you don't have a real mail account for testing
+      let testAccount = await nodemailer.createTestAccount();
+
+      // create reusable transporter object using the default SMTP transport
+      let transporter = nodemailer.createTransport({
+        host: 'smtp.ethereal.email',
+        port: 587,
+        secure: false, // true for 465, false for other ports
+        auth: {
+          user: testAccount.user, // generated ethereal user
+          pass: testAccount.pass, // generated ethereal password
+        },
+      });
+
+      // send mail with defined transport object
+      let info = await transporter.sendMail({
+        from, // sender address
+        to, // list of receivers
+        subject: 'Červený Kláštor | Vaša objednávka bola prijatá', // Subject line
+        text: 'Hello world?', // plain text body
+        html: '<b>Hello world?</b>', // html body
+      });
+
+      console.log('Message sent: %s', info.messageId);
+      resolve();
+    } catch (err) {
+      reject(err);
+    }
+  });
+}
 
 const createOrder: (root: any, args: any, ctx: any) => Promise<String> = async (
   root,
@@ -11,6 +50,8 @@ const createOrder: (root: any, args: any, ctx: any) => Promise<String> = async (
   const newOrder: IOrder = new Order(data);
 
   await Order.create(newOrder);
+
+  await sendMailNotification('info@codebrothers.sk', data.email);
 
   const { userId } = data;
 
