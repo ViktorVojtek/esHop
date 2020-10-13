8db3655801d0d279cd2a29bb1ccd52dc0353bd34
stephanides <stefanmacuga1@gmail.com>
2020-10-14 00:53:52 +0200

    fixed delete customer account

--- server/app-data/graphql/resolvers/mutation/RemoveCustomer/index.ts
+++ server/app-data/graphql/resolvers/mutation/RemoveCustomer/index.ts
@@ -1,16 +1,15 @@
-import * as mongoose from 'mongoose';
+import mongoose from 'mongoose';
 import Customer, { ICustomer } from '../../../../db/models/Customer';
 import ModError from '../../utils/error';
 
 export default async (root: any, args: { id: string }, ctx: any) => {
   const { id } = args;
 
-  console.log('test id' + id);
   const customer: ICustomer = await Customer.findOne({
     _id: mongoose.Types.ObjectId(id),
   });
 
-  console.log(customer);
+  console.log("this is customer" + customer);
 
   if (!customer) {
     throw new ModError(404, 'Customer not exist');
