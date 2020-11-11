4dc1b05bfe14e29fc2c27b2b9d31766919690dd9
stephanides <stefanmacuga1@gmail.com>
2020-11-11 17:57:06 +0100

    WIP: payment gateway integration

--- server/app-data/controllers/payment/index.ts
+++ server/app-data/controllers/payment/index.ts
@@ -6,7 +6,12 @@ import { calculateOrderId } from '../../graphql/resolvers/utils';
 
 var nodeNestpay = require('node-nestpay');
 
-export default async (orderData: any, req: Request, res: Response, next: NextFunction) => {
+export default async (
+  orderData: any,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   const {
     gp: { ClientID, ClientName, ClientPass, StoreKey },
   } = config;
@@ -14,18 +19,22 @@ export default async (orderData: any, req: Request, res: Response, next: NextFun
   const amount: number = orderData.totalPrice; // total sum of transaction
   const currency: number = 978;
   const oid: string = orderId; // unique identifier of order
-  const okUrl: string = 'http://localhost:3016/payment/success';
-  const failUrl: string = 'http://localhost:3016/payment/error';
+  const okUrl: string =
+    'https://eshop.kupelecks.sk/eshop/cart/uspesna-objednavka';
+  const failUrl: string =
+    'https://eshop.kupelecks.sk/eshop/cart/neuspesna-objednavka';
   const tranType: string = 'Auth';
 
-  const plainText = `${ClientID}|${oid}|${amount}|${okUrl}|${failUrl}|${tranType}||||${currency}|${StoreKey}`;
+  const plainText = `${ClientID}|${oid}|${amount}|${okUrl}|${failUrl}|${tranType}||"asdf"||||${currency}|${StoreKey}|`;
+
+  console.log(plainText);
 
   const hash = crypto.createHash('sha512');
-
   const hashedData = hash.update(plainText);
   const genHash = hashedData.digest('hex');
-
   const base64 = Buffer.from(genHash).toString('base64');
+
+  console.log(base64);
 
   //const base64Buff: Buffer = Buffer.from(genHash, 'base64');
   //const base64 = base64Buff.toString('base64');
@@ -34,16 +43,21 @@ export default async (orderData: any, req: Request, res: Response, next: NextFun
 
   const data = {
     clientid: ClientID,
-    storetype: '3d_pay_hosting',
+    storetype: '3D_PAY_HOSTING',
     hash: base64,
+    trantype: tranType,
     amount,
     currency: currency,
     oid: orderId,
     okUrl,
     failUrl,
     lang: 'sk',
+    rnd: 'asdf',
+    hashAlgorithm: 'ver2',
     encoding: 'utf-8',
   };
+
+  console.log(data);
 
   try {
     const response = await fetch(
@@ -56,26 +70,13 @@ export default async (orderData: any, req: Request, res: Response, next: NextFun
         method: 'POST',
       }
     );
-  
-    console.log(response);
-    /*const nestpay = new nodeNestpay({
-      name: '',
-      password: '',
-      clientId: ClientID,
-      storekey: '3d_pay_hosting',
-      callbackSuccess: okUrl,
-      callbackFail: failUrl,
-      currency: currency,
-      orderId: orderId,
-      endpoint: 'asseco',
-    }) as any;
-  
-    console.log(nestpay);*/
     const respJson = await response.text();
 
     console.log('\n');
     console.log(respJson);
 
     res.json({ message: respJson });
-  } catch (err) { next(err); }
+  } catch (err) {
+    next(err);
+  }
 };
