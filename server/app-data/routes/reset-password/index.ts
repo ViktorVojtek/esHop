d9485b1a545282a3f827f136e138c6ef3f0fa8d1
stephanides <stefanmacuga1@gmail.com>
2020-11-29 22:09:57 +0100

    hotfix

--- server/app-data/routes/reset-password/index.ts
+++ server/app-data/routes/reset-password/index.ts
@@ -2,19 +2,15 @@ import { Request, Response, NextFunction } from 'express';
 import Customer, { ICustomer } from '../../db/models/Customer';
 import crypto from 'crypto';
 import nodemailer from 'nodemailer';
+import { config } from '../../config';
 
-const mailTransporter = nodemailer.createTransport({
-  host: 'smtp.websupport.sk',
-  port: 465, // 587,
-  secure: true, // true, // ssl
-  auth: {
-    user: process.env.EMAIL_LOGIN, // generated ethereal user
-    pass: process.env.EMAIL_PASS, // generated ethereal password
-  },
-});
-
-export const resetPasswordRoute = async (req: Request, res: Response, next: NextFunction) => {
-  const { body: email } = req
+export const resetPasswordRoute = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const { mailer } = config;
+  const { email } = req.body;
   const domain: string =
     process.env.NODE_ENV === 'production'
       ? 'eshop.kupelecks.sk'
@@ -24,9 +20,21 @@ export const resetPasswordRoute = async (req: Request, res: Response, next: Next
 
   if (!customer) {
     return res
-        .status(404)
-        .send({ msg: 'We were unable to find a user for this token.' });
+      .status(404)
+      .send({ msg: 'We were unable to find a user for this token.' });
   }
+
+  const mailTransporter = nodemailer.createTransport({
+    host: mailer.host,
+    port: mailer.port,
+    secure: mailer.secure,
+    auth: {
+      user: mailer.auth.user,
+      pass: mailer.auth.pass,
+    },
+  });
+
+  console.log(mailTransporter);
 
   const oneDay: number = 24 * 60 * 60 * 1000;
   const date: number = Date.now() + oneDay;
@@ -54,7 +62,7 @@ export const resetPasswordRoute = async (req: Request, res: Response, next: Next
     await mailTransporter.sendMail(mailOptions);
 
     res.send('A reset password email has been sent to ' + email + '.');
-  } catch(error) {
+  } catch (error) {
     return next(error);
   }
 };
